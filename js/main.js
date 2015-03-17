3a0c7b1563be38e072f39346c69dafd0f92bf1fc
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-03-17 18:10:33 +0700

    Ver 3: 70% complete

---
+++ js/main.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+	// slickcarousel
+	$('#slideshow').slick({
+		autoplay: true,
+		infinite: true,
+	  speed: 500,
+	  fade: true,
+	  cssEase: 'linear',
+	  prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+	  nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>'
+	});
+
+	// Filter portfolio
+	$container = $('.portfolios');
+	$container.isotope({
+		itemSelector: '.filter-item'
+	});
+
+	var $filterButton = $('.filter-buttons');
+
+	$filterButton.on('click', 'a', function(e){
+		e.preventDefault();
+		var $this = $(this);
+		$filterButton.find('a').removeClass('active');
+		$this.addClass('active');
+		var filterValue = $this.data('filter');
+  	$container.isotope({ filter: filterValue });
+	});
+});
